788d286d7cc6c22e424e14bf45e95f235a813f00
Paulo Eduardo Santos de Sousa <eduardo.lwx@gmail.com>
2020-06-20 21:19:51 -0300

    Test circleci with test fail

--- app/src/androidTest/java/com/eduardolwx/mangareader/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/eduardolwx/mangareader/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.eduardolwx.mangareader", appContext.packageName)
+        assertEquals("com.eduardolwx.mangareader_", appContext.packageName)
     }
 }
